f97c64f86cdc319758215d88f93dcf9c675c0ad8
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-22 14:00:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'İbrahim Paşa Hamamı / İbrahim Pasha Bathhouse',
+    title: 'İbrahim Paşa Hamamı / İbrahim Pasha Bath',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
